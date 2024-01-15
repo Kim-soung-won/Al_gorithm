a7fc5be4fc782c7add2d5b771b51cdf00e8a9b8e
Kim-seng-won <105148570+Kim-seng-won@users.noreply.github.com>
2024-01-15 09:56:54 +0900

    [Gold V] Title: 강의실 배정, Time: 736 ms, Memory: 69112 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Time{
+	int start;
+	int end;
+	public Time(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int loop = Integer.parseInt(br.readLine());
+		Time [] times = new Time[loop]; 
+		for(int i=0; i<loop; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			times[i] = new Time(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())); 
+		}
+		Arrays.sort(times, new Comparator<Time>() {
+			@Override
+			public int compare(Time o1, Time o2) {
+				if(o1.start!=o2.start) return o1.start - o2.start;
+				else return o1.end - o2.end;
+			}
+		});
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(times[0].end);
+		for(int i=1; i<loop; i++) {
+			if(times[i].start>=pq.peek()) {
+				pq.poll();
+				pq.add(times[i].end);
+			}
+			else {
+				pq.add(times[i].end);
+			}
+		}
+		System.out.println(pq.size());
+	}
+}
