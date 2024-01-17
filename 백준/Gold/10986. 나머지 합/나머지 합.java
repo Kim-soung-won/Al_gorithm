7c7cef1f61da6d71d98264bee9d3ad3b0229070b
Kim-seng-won <105148570+Kim-seng-won@users.noreply.github.com>
2024-01-17 10:33:58 +0900

    [Gold III] Title: 나머지 합, Time: 524 ms, Memory: 120676 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -10,15 +10,14 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		final int N = Integer.parseInt(st.nextToken());
 		final int M = Integer.parseInt(st.nextToken());
-		long sumArr[] = new long[N];
+		long sumArr[] = new long[N+1];
 		long c[] = new long[M];
 		long count = 0;
 		st = new StringTokenizer(br.readLine());
-        sumArr[0] = Integer.parseInt(st.nextToken());
-		for(int i=1; i<N; i++) {
+		for(int i=1; i<=N; i++) {
 			sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
 		}
-		for(int i=0; i<N; i++) {
+		for(int i=1; i<=N; i++) {
 			int rere = (int) (sumArr[i]%M);
 			if(rere==0) {
 				count++;
