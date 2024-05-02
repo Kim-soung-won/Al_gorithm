bee2772e9f755990371bb0879f3b3e68e7ad2581
Kim soung won <105148570+Kim-soung-won@users.noreply.github.com>
2024-05-02 12:34:03 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,9 +1,4 @@
 def solution(s):
-    count = set()
     if s == 1:
-        return s
-    for i in range(1, int(s/2)+1):
-        if s%i == 0:
-            count.add(i)
-            count.add(int(s/i))
-    return sum(count)
+        return 1
+    return s + sum([i for i in range(1, s//2 + 1) if s % i == 0])
