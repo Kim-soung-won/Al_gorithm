3a281b4b763a25d67d788e892650cc42fe162665
Kim soung won <105148570+Kim-soung-won@users.noreply.github.com>
2024-05-12 01:25:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,14 +1,3 @@
 def solution(s):
-    arr: list[str] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    check: str = ""
-    answer: str = ""
-    for i in range(len(s)):
-        if s[i].isnumeric():
-            answer += s[i]
-        else:
-            check += s[i]
-        if check in arr:
-            answer += str(arr.index(check))
-            print("index = ", arr.index(check))
-            check = ""
-    return int(answer)
+    answer = 0
+    return answer
