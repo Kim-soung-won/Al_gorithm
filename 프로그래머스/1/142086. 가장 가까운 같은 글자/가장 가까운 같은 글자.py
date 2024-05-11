625800d3ba932291315cfd5f2e78df15f7e4488c
Kim soung won <105148570+Kim-soung-won@users.noreply.github.com>
2024-05-12 02:24:55 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.60 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,11 +1,3 @@
 def solution(s):
-    arr: dict[str, int] = {}
-    answer: list[int] = []
-    for i in range(len(s)):
-        if arr.get(s[i]) is not None:
-            answer.append(i-arr.get(s[i]))
-            arr[s[i]] = i
-        else:
-            answer.append(-1)
-            arr[s[i]] = i
+    answer = []
     return answer
