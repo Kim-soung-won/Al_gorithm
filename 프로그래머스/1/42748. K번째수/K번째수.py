c0f9ded41d74b8b9e9c594a5095f6816bbeb7e75
Kim soung won <105148570+Kim-soung-won@users.noreply.github.com>
2024-05-14 10:23:56 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,6 @@
 def solution(array, commands):
     answer: list[int] = []
-    for i in range(len(commands)):
-        arr: list[int] = array[commands[i][0] - 1:commands[i][1]]
-        arr.sort()
-        print("arr : ", arr)
-        print("value : ", commands[i][2])
-        answer.append(arr[commands[i][2] - 1])
-        print("answer : ", answer)
+    for command in commands:
+        l, j, k = command
+        answer.append(list(sorted(array[l - 1:j]))[k - 1])
     return answer
