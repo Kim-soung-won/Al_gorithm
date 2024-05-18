d43b77098c2cb4b4000dc7e184812fad70c85251
Kim soung won <105148570+Kim-soung-won@users.noreply.github.com>
2024-05-19 00:31:28 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,8 +1,8 @@
 def solution(name, yearning, photo):
     answer: list[int] = []
-    for i in range(len(photo)):
+    for i in photo:
         count: int = 0
-        for j in photo[i]:
+        for j in i:
             if name.__contains__(j):
                 count += yearning[name.index(j)]
         answer.append(count)
